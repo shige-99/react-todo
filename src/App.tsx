77b3785f8049acc3dbc8b610baf0bfef4b01b6e7
shige99 <shige992306@gmail.com>
2024-07-02 23:12:33 +0900

    refactor: make componentsfile for TodoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { TodoList } from "./components/TodoList";
 import { dummyTodoList } from "./data/dummyTodoList";
 
 function App() {
@@ -6,9 +7,7 @@ function App() {
       <h1 className="text-center text-4xl">Todoアプリ</h1>
       <div className="rounded bg-slate-200 p-5">
         <div className="space-y-3">
-          {dummyTodoList.map((todo) => (
-            <p key={todo.id}>{todo.title}</p>
-          ))}
+          <TodoList todoList={dummyTodoList} />
         </div>
       </div>
     </main>
